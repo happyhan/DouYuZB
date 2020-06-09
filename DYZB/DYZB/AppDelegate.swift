dde64c8da1b98dc012a5d5de2a1d27c420ae5870
Yuels <lingshey@ptm.com.cn>
2020-06-09 13:28:03 +0800

    1.初始化项目基于Xcode11

---
+++ DYZB/DYZB/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  DYZB
+//
+//  Created by Yuels on 2020/6/9.
+//  Copyright © 2020 Portman Electronics Co.Ltd. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+	var window: UIWindow?
+
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+		// Override point for customization after application launch.
+		return true
+	}
+}
+
---
+++ DYZB/DYZB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DYZB
+//
+//  Created by Yuels on 2020/6/9.
+//  Copyright © 2020 Portman Electronics Co.Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
